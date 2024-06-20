f0f18d008a0f3e94ee34dfdf52820ca45970fbb6
SmartKJOSHUA <kisakyeoligachy@gmail.com>
2024-06-20 13:37:51 +0330

    Create num21.py

---
+++ num21.py
@@ -0,0 +1,10 @@
+def check_driving_age():
+    age = int(input("Enter your age: "))
+    if age >= 18:
+        print("You are old enough to learn to drive.")
+    else:
+        years_left = 18 - age
+        print(f"You need {years_left} more years to learn to drive.")
+
+
+check_driving_age()
