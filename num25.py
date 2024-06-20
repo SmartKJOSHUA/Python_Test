0b4ad609f084869d33b14469137d898a4beaba0a
SmartKJOSHUA <kisakyeoligachy@gmail.com>
2024-06-20 13:40:10 +0330

    Create num25.py

---
+++ num25.py
@@ -0,0 +1,11 @@
+student = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'gender': 'Male',
+    'age': 20,
+    'marital_status': 'Single',
+    'skills': ['Python', 'Java', 'SQL'],
+    'country': 'USA',
+    'city': 'New York',
+    'address': '123 Main St'
+}
