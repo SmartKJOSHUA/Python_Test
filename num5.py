7713a66d3530eabfcd653c06878e2eb306b8e0f8
SmartKJOSHUA <kisakyeoligachy@gmail.com>
2024-06-20 13:29:20 +0330

    Create num5.py

---
+++ num5.py
@@ -0,0 +1,12 @@
+def check_season(month):
+    if month in [3, 4, 5]:
+        return 'Spring'
+    elif month in [6, 7, 8]:
+        return 'Summer'
+    elif month in [9, 10, 11]:
+        return 'Autumn'
+    else:
+        return 'Winter'
+
+
+print(check_season(7))  
